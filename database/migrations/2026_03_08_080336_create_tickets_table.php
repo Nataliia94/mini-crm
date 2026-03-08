33f12691cab250060bddf91a8645ef16a3893683
Natalia <kornchak365@gmail.com>
2026-03-08 10:35:08 +0200

    Add migrations for customers and tickets

--- database/migrations/2026_03_08_080336_create_tickets_table.php
+++ database/migrations/2026_03_08_080336_create_tickets_table.php
@@ -13,6 +13,17 @@ return new class extends Migration
     {
         Schema::create('tickets', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('customer_id')
+                  ->constrained()
+                  ->cascadeOnDelete();
+            $table->string('subject');
+            $table->string('text');
+            $table->enum('status', [
+                  'new',
+                  'in_progress',
+                  'processed'
+            ])->default('new');
+            $table->timestamp('manager_reply_at')->nullable();      
             $table->timestamps();
         });
     }
